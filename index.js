3b97cbbc9963616c906919413c4d229e946babe0
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-04 23:06:46 -0700

    finish the promise

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 const dogDiv = document.querySelector(".dog");
 const dogPic =  document.createElement('img');
 
-//create a way to get the data from the dog api.
+//create a way to get the picture file from the dog api.
 
 dogURL = "https://dog.ceo/api/breeds/image/random";
 
 
 fetch(dogURL)
     .then (response => response.json())
-    .then(data => console.log(`"${data.message}"`));
+    .then(data => {    
+        dogPic.src = `${data.message}` //turns out I just had an extra set of quotations!
+        dogDiv.append(dogPic);
+    }
+);
 
-    //dogPic.src = `"${data.message}"`
-    //dogPic.src = "https://images.dog.ceo/breeds/malamute/n02110063_650.jpg";
-    dogDiv.append(dogPic);
-
-    //file:///Users/casvalkyriespicer/Documents/GitHub/Dog-API/%22   https://images.dog.ceo/breeds/setter-english/n02100735_861.jpg %22
+  
