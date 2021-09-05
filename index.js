47b433c8668726725664b92d77592cc61ee9c1f8
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-04 20:10:37 -0700

    add some img rendering basics

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 //grab elements from DOM
 
 const dogDiv = document.querySelector(".dog");
+const dogPic =  document.createElement('img');
 
 //create a way to get the data from the dog api.
 
-fetch( "https://dog.ceo/api/breeds/image/random")
+dogURL = "https://dog.ceo/api/breeds/image/random";
+
+
+fetch(dogURL)
     .then (response => response.json())
-    .then(data => console.log(data));
+    .then(data => console.log(`"${data.message}"`));
+
+    //dogPic.src = `"${data.message}"`
+    //dogPic.src = "https://images.dog.ceo/breeds/malamute/n02110063_650.jpg";
+    dogDiv.append(dogPic);
+
+    //file:///Users/casvalkyriespicer/Documents/GitHub/Dog-API/%22   https://images.dog.ceo/breeds/setter-english/n02100735_861.jpg %22
